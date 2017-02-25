56a5b13e2229b2a626cf87500aeb1fd43acc0f65
Robert White <rwhite@chiefwhitecloud.com>
2017-02-24 21:48:50 -0330

    Added last updated to race when created

--- database/database.go
+++ database/database.go
@@ -134,7 +134,7 @@ func (db *Db) Open() error {
 }
 
 func (db *Db) CreateImportTask(url string) (ImportTask, error) {
-	race := Race{Name: "Pending", ImportStatus: "pending", SrcUrl: url}
+	race := Race{Name: "Pending", ImportStatus: "pending", SrcUrl: url, LastUpdated: time.Now()}
 	err := db.orm.Create(&race).Error
 	if err != nil {
 		log.Printf("%s", err)
